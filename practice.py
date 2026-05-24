c015561503929c64877b4b3eaa90e92d27af1c6c
Aleja <paulagarro@unisabana.edu.co>
2026-05-24 15:03:57 -0500

    one

---
+++ practice.py
@@ -0,0 +1,5 @@
+from song import Song
+
+def print_songs(song_list):
+    for song in song_list:
+        print(song)
--- song.py
+++ song.py
@@ -1,1 +1,12 @@
 # Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = str(name)
+        self.artist = str(artist)
+        self.length = float(length)
+
+    def get_length_in_seconds(self):
+        return self.length * 60.0
+
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
